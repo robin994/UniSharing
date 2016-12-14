ee04d0660aca2e0b3b0dc06076b1af2e22cb532e
annatomeo <anna_tomeo@alice.it>
2016-12-14 09:45:46 +0100

    segnalazione corretta by anna

---
+++ contact/index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>UniSharing</title>
+        <link href="../css/bootstrap.css" rel="stylesheet" media="screen">
+        <link href="../css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="../css/style.css" rel="stylesheet" media="screen">
+				<script src="http://code.jquery.com/jquery-1.12.2.min.js"></script>
+				<script src="../js/bootstrap.min.js"></script>
+	</head>
+	<body>
+
+		<header>
+				<nav class="navbar navbar-default navbar-static-top">
+						<div class="container">
+								<div class="navbar-header">
+										<a href="index.html" class="navbar-brand">UniSharing</a>
+										<button class="navbar-toggle" data-toggle="collapse" data-target="#navHeaderCollapse" >
+												<span class="icon-bar"></span>
+												<span class="icon-bar"></span>
+												<span class="icon-bar"></span>
+										</button>
+								</div>
+								<div class="collapse navbar-collapse" id="navHeaderCollapse">
+										<ul class= "nav navbar-nav navbar-right">
+												<li class="active"><a href="index.html">Home</a></li>
+												<li><a href="">Profilo</a></li>
+												<li><a href="">Lista nera</a></li>
+												<li class="dropdown">
+														<a href="" class="dropdown-toggle" data-toggle="dropdown">Gruppi <b class="caret"></b></a>
+														<ul class="dropdown-menu">
+																<li><a href="">A cui partcipo</a></li>
+																<li><a href="">Di cui sono amministratore</a></li>
+														</ul>
+												 </li>
+												<li><a href=""> Segnalazione</a></li>
+										</ul>
+								</div>
+						</div>
+				</nav>
+		</header>
+<div class="contaniner">
+
+
+	<div class="col-lg-2">
+
+		</div>
+
+		<div class="col-lg-8">
+				<h1>Segnalazione</h1>
+
+				<label>Titolo</label>
+
+				<div class ="input-group" style="width:100%;">
+					<input type="text" class ="form-control" placeholder="">
+				</div><br>
+				<label>Testo</label><br>
+				<div class ="input-group" style="width:100%;">
+				<textarea class="form-control" rows="10"></textarea>
+			</div><br>
+				<center>
+					<div class ="input-group">
+						<button  type="button" class="btn btn-default btn-lg">Annulla</button>&nbsp;
+						<button  type="button " class="btn btn-default btn-lg">Invia</button>
+					</div>
+				</center>
+		</div>
+
+		<div class="col-lg-2">
+		</div>
+
+	</div>
+</div>
+
+
+
+    <footer>
+
+    </footer>
+
+	</body>
+</html>
