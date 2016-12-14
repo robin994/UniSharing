eb2d3559b095a68403cdad270541c4b6672988f9
xGiuseppe95 <giuseppe.altobelli.95@gmail.com>
2016-12-14 12:23:33 +0100

    commit per lorenzo

--- storage/proxy.php
+++ storage/proxy.php
@@ -9,7 +9,6 @@ include "../students/php/User.php";
 //if(is_file($_POST['classe'].".php"))
 //	include $_POST['classe'].".php";
 
-
 $obj = new $_POST['classe']();
 $obj->init();
 echo $obj->$_POST['metodo']($_POST);
