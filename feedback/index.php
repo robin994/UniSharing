e8f3a64e7a3494b4520e2e7af108a6a9f56814e9
lorenzdev <lorenzovitale@MacBook-Pro.local>
2016-12-21 08:04:44 +0100

    commit ok

--- feedback/index.php
+++ feedback/index.php
@@ -1,182 +1,62 @@
+<?
+
+
+// verifico che l'utente sia loggato
+
+
+
+?>
+
 <!doctype html>
 <html>
 	<head>
 		<meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>UniSharing</title>
+		<title>Unisharing</title>
 		<script src="../../js/jquery.1.12.js"></script>
 		<link href="../../css/bootstrap.css" rel="stylesheet" media="screen">
 		<link href="../../css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<script src="../js/bootstrap.min.js"></script>
 		<link href="../../css/style.css" rel="stylesheet" media="screen">
 		<link rel="stylesheet"	type="text/css" href="../../js/jquery-confirm-master/jquery-confirm.min.css"/>
-	  <script type="text/javascript" src="../../js/jquery-confirm-master/jquery-confirm.min.js"></script>
+	  	<script type="text/javascript" src="../../js/jquery-confirm-master/jquery-confirm.min.js"></script>
 		<script type="text/javascript" src="../../js/functions.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+		<script>
 
-
-<script>
+		var mask_feedback = "";
 
 		// ONLOAD JQUERY
 		$(function(){
-
-			var mask_feedback = "";
-
+			
+			
+			
+			
+			// carico Mark_feedback
 			$.get("htmls/index.html", function(html){
 					
-					
-						mask_feedback = html;
-					
-						//var idGruppo = "1";
-						//var userLoggato = "tester1@unisharing.it"; //tester1
+				mask_feedback = html;
+				//var idGruppo = "1";
+				//var userLoggato = "tester1@unisharing.it"; //tester1
 			
-						var idGruppo = "<?php echo $_GET["g"]; ?>";
-						var userLoggato = "<?php echo $_GET["u"]; ?>";
+				var idGruppo = "<?php echo $_GET["g"]; ?>";
+				var userLoggato = "<?php echo $_GET["u"]; ?>";
 			
-						function callBackCheckFeedback(data){
-				
-							console.log("Recevo dati dei feedback");
-							console.log(data);
-						
-							if(!data.success){
-								alert("Errore! " + data.errorMessage);
-								return;
-							}
-							
-							
-							
-							
-							if(data.results.length > 0){
-								
-								
-								// Visualizzo il messaggio di conferma per l'inserimento dei feedback
-								$.confirm({
-								title: 'Complimenti, hai completato un rapporto di studi',
-								content: 'Desideri lasciare un feedback per gli utenti che hanno partecipato al gruppo?',
-								buttons: {
-									confirm: function () {
-										
-									
-									$("#Mask_feedback").append('<center><br><div class="alert alert-success" style="font-size:34px;"><i class="glyphicon glyphicon-ok" style="font-size:22px;"/><h2>Congratulazioni</h2><h3>Ancora una volta hai portato a termine una nuova esperienza, vuoi raccontarci com\'è andata?</h3></div>');
-								
-									for(var i = 0;i < data.results.length;i++){
-										
-										var mask_feed = $.parseHTML(html);
-										
-										$(mask_feed).attr("groupId", data.results[i].group);
-										$(mask_feed).attr("account", data.results[i].account);
-										
-										$(mask_feed).find(".optradio_1").attr("name", "_"+i+"optradio_1");
-										$(mask_feed).find(".optradio_2").attr("name", "_"+i+"optradio_2");
-										$(mask_feed).find(".optradio_3").attr("name", "_"+i+"optradio_3");
-										$(mask_feed).find(".optradio_4").attr("name", "_"+i+"optradio_4");
-										
-										$("#Mask_feedback").append("<h3>Come si è comportato "+ data.results[i].name + " " + data.results[i].surname+"?");
-										$("#Mask_feedback").append(mask_feed);
-									}
-									
-									$("#Mask_feedback").append('<center><a class="bottoneInvioFeedback"><button type="button" class="btn btn-primary btn-lg">Salva Feedback</button></a></center><br><br>');
-									
-									
-									
-									////////////////////////////////////////////////////////////////////////
-									/////////////// FUNZIONE DI CLICK CHE AVVIA IL SALVATAGGIO /////////////
-									////////////////////////////////////////////////////////////////////////
-									
-									$(".bottoneInvioFeedback").on("click", function(){
-										
-										
-										var param = {
-											"feedbacks": []	,
-											"group": "",
-											"author": userLoggato
-										}
-										
-										$(".Feedback").each(function(){
-											
-											var user = $(this).attr("account");
-											var gruppo = $(this).attr("groupId");
-											
-											param.group = gruppo;
-											
-											var feed1 = $(this).find(".optradio_1:checked").val();
-											var feed2 = $(this).find(".optradio_2:checked").val();
-											var feed3 = $(this).find(".optradio_3:checked").val();
-											var feed4 = $(this).find(".optradio_4:checked").val();
-											var comment = $(this).find(".comment").val();
-											
-											var score = parseInt(feed1) + parseInt(feed2) + parseInt(feed3) + parseInt(feed4);
-											
-											var tmp_feed = {
-												"feed1": feed1,
-												"feed2": feed2,
-												"feed3": feed3,
-												"feed4": feed4,
-												"user": user,
-												"score": score,
-												"comment": comment
-											}
-											
-											param.feedbacks.push(tmp_feed);
-										});
-										
-										
-										console.log("ECCO I FEEDBACK");
-										console.log(param);
-										
-										///////////////////////////////////////////////////////////////////////////////
-										/////////////// METODO DELLA CLASSE FEEDBACK CHE SALVA I FEEDBACK /////////////
-										///////////////////////////////////////////////////////////////////////////////
-										
-										function callBackFeedback(data){	
-											console.log(data);
-	
-											if(!data.success){
-												alert("Errore! " + data.errorMessage);
-												return;
-											}
-											
-											
-											$("#Mask_feedback").html('<center><br><div class="alert alert-success"><i class="glyphicon glyphicon-ok" style="font-size:34px;"/><br><br><span style="font-size:18px;">Feedback inseriti correttamente</span></div></center>');
-											
-											
-										}
-										
-										$.unisharing("Feedback", "insertFeedback", "private", param, false, callBackFeedback);
-										
-									});
-								
-								},
-								
-							  cancel: function(){
-									 
-							  }
-						  }
-					  });		
-													
-								
-							}else{
-								
-								var tmp = '<center><br><div class="alert alert-success"><i class="glyphicon glyphicon-ok" style="font-size:34px;"/><br><br><span style="font-size:18px;">Complimenti, hai terminato il rapporto di studi.<br>Hai inserito correttamente i feedback per ogni compagno di studi!</span></div></center>';
-								$("#Mask_feedback").html(tmp);
-								
-							}
-						}
+				// verifico se esistono dei feedback da inserire 
+				checkFeedback(idGruppo, userLoggato);
 			
-						$.unisharing("Feedback", "checkFeedback", "private", {"gruppo": idGruppo, "user": userLoggato}, false, callBackCheckFeedback);
 			});
 		
 		});
 			
 		</script>
-
     </head>
 	<body>
-
         <header>
             <nav class="navbar navbar-default navbar-static-top">
                 <div class="container">
-								    <div class="navbar-header">
-                        <a href="index.html" class="navbar-brand">UniSharing</a>
+					<div class="navbar-header">
+                        <a href="index.html" class="navbar-brand" style="margin-top: -12px;"><img src="../img/logo.png" width="150"/></a>
                         <button class="navbar-toggle" data-toggle="collapse" data-target="#navHeaderCollapse" >
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
@@ -203,21 +83,12 @@
         </header>
 
         <div class="container">
-
        		<div class= "row">
-						<div class= "col-lg-2">
-						</div>
-						<div class= "col-lg-8" id="Mask_feedback">
-							
-						</div>
-						<div class= "col-lg-2">
-						</div>
-      </div>
-
-
-</div>
-        <footer>
-        </footer>
-
+				<div class= "col-lg-2"></div>
+				<div class= "col-lg-8" id="Mask_feedback"></div>
+				<div class= "col-lg-2"></div>
+      		</div>
+		</div>
+        <footer></footer>
 	</body>
 </html>
---
+++ feedback/js/main.js
@@ -0,0 +1,163 @@
+// Funzioni FEEDBACK
+
+////////////////////////////////////////////////
+//////// Funzione che inserisce i feedback /////
+////////////////////////////////////////////////
+
+function checkFeedback(idGruppo, userLoggato){
+	
+	// funzione di callback
+	function callBackCheckFeedback(data){
+		
+		console.log("Recevo dati dei feedback");
+		console.log(data);
+					
+		if(!data.success){
+			alert("Errore! " + data.errorMessage);
+			return;
+		}
+						
+		// verifico se ci sono feedback da inserire
+		if(data.results.length > 0){
+							
+			var title = '<center>';
+				title += '<i class="glyphicon glyphicon-warning" style="font-size:22px;"/><br>';
+				title += '<h3>Complimenti</h3>hai completato un rapporto di studi';
+				title += '</center>';				
+							
+			// Visualizzo il messaggio di conferma per l'inserimento dei feedback
+			$.confirm({
+				title: title,
+				content: 'Desideri lasciare un feedback per gli utenti che hanno partecipato al gruppo?',
+				buttons: {
+					confirm: function () {
+						
+						var tmp = '<div class="alert alert-success">';
+							//tmp += '<i class="glyphicon glyphicon-ok" style="font-size:22px;"/>';
+							//tmp += '<h2>Congratulazioni</h2>';
+							tmp += 'Ancora una volta hai portato a termine una nuova esperienza, vuoi raccontarci com\'è andata?';
+							tmp += '</div>';
+						
+						$("#Mask_feedback").append(tmp);
+								
+						for(var i = 0;i < data.results.length;i++){
+							var mask_feed = $.parseHTML(mask_feedback);
+							$(mask_feed).attr("groupId", data.results[i].group);
+							$(mask_feed).attr("account", data.results[i].account);
+							$(mask_feed).find(".optradio_1").attr("name", "_"+i+"optradio_1");
+							$(mask_feed).find(".optradio_2").attr("name", "_"+i+"optradio_2");
+							$(mask_feed).find(".optradio_3").attr("name", "_"+i+"optradio_3");
+							$(mask_feed).find(".optradio_4").attr("name", "_"+i+"optradio_4");
+							$("#Mask_feedback").append("<h3>Come si è comportato "+ data.results[i].name + " " + data.results[i].surname+"?");
+							$("#Mask_feedback").append(mask_feed);
+						}
+						
+						var tmpBtn = '<center>';
+							tmpBtn += '<a class="bottoneInvioFeedback">';
+							tmpBtn += '<button type="button" class="btn btn-primary btn-lg">Salva Feedback</button>';
+							tmpBtn += '</a>';
+							tmpBtn += '</center>';
+							tmpBtn += '<br><br>';
+						
+						$("#Mask_feedback").append(tmpBtn);
+									
+						////////////////////////////////////////////////////////////////////////
+						/////////////// FUNZIONE DI CLICK CHE AVVIA IL SALVATAGGIO /////////////
+						////////////////////////////////////////////////////////////////////////
+						$(".bottoneInvioFeedback").on("click", function(){
+								
+							var param = {
+								"feedbacks": [],
+								"group": "",
+								"author": userLoggato
+							}
+								
+							$(".Feedback").each(function(){
+									
+								var user = $(this).attr("account");
+								var gruppo = $(this).attr("groupId");
+									
+								param.group = gruppo;
+									
+								var feed1 = $(this).find(".optradio_1:checked").val();
+								var feed2 = $(this).find(".optradio_2:checked").val();
+								var feed3 = $(this).find(".optradio_3:checked").val();
+								var feed4 = $(this).find(".optradio_4:checked").val();
+								var comment = $(this).find(".comment").val();
+									
+								var score = parseInt(feed1) + parseInt(feed2) + parseInt(feed3) + parseInt(feed4);
+									
+								var tmp_feed = {
+									"feed1": feed1,
+									"feed2": feed2,
+									"feed3": feed3,
+									"feed4": feed4,
+									"user": user,
+									"score": score,
+									"comment": comment
+								}
+									
+								param.feedbacks.push(tmp_feed);
+							});
+							
+							
+							// metodo che implementa l'inserimento dei feedback
+							insertFeedback(param);
+							
+						});
+								
+					},
+				
+					cancel: function(){}
+				}
+		});		
+												
+							
+	}else{
+							
+		var tmp = '<center><br>';
+			tmp += '<div class="alert alert-success">';
+			tmp += '<i class="glyphicon glyphicon-ok"/> ';
+			tmp += '<span style="font-size:18px;">Ha già inseriti i feedback per questo gruppo</span>';
+			tmp += '</div>';
+			tmp += '</center>';
+			
+			$("#Mask_feedback").html(tmp);
+							
+			}
+		}
+		
+		$.unisharing("Feedback", "checkFeedback", "private", {"gruppo": idGruppo, "user": userLoggato}, false, callBackCheckFeedback);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////// METODO DELLA CLASSE FEEDBACK CHE SALVA I FEEDBACK /////////////
+///////////////////////////////////////////////////////////////////////////////
+
+function insertFeedback(feed){
+				
+	function callBackFeedback(data){	
+		console.log(data);
+
+		if(!data.success){
+			alert("Errore! " + data.errorMessage);
+			return;
+		}
+					
+		var tmpFeed = '<center>';
+			tmpFeed += '<br>';
+			tmpFeed += '<div class="alert alert-success">';
+			tmpFeed += '<i class="glyphicon glyphicon-ok" style="font-size:34px;"/><br><br>';
+			tmpFeed += '<span style="font-size:18px;">Feedback inseriti correttamente</span>';
+			tmpFeed += '</div>';
+			tmpFeed += '</center>';		
+					
+		$("#Mask_feedback").html(tmpFeed);
+									
+	}
+	
+	// invoco il metodo di insertFeedback
+	$.unisharing("Feedback", "insertFeedback", "private", feed, false, callBackFeedback);	
+	
+}
