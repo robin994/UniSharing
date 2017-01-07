95213300d1a0eb8493f6a67de1a877d23940d38d
Roberto <robin994@hotmail.it>
2017-01-07 16:19:48 +0100

    asd

--- php/footer.php
+++ php/footer.php
@@ -2,17 +2,17 @@
 <footer class="footer">
 	<div class="footerContainer">
 		<span class="text-muted">Copyright © 2016 UniSharing. All rights reserved.</span>
-    </div>
+	</div>
 </footer>
 
 <script>
 
 $(function(){
-	
+
 	$(window).scroll(function(){
 		$(".footer").css({position: "fixed", bottom: "0px"});
 	});
-	
+
 });
 
 </script>
